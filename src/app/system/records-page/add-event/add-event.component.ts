f2f45eea73a2491e32fb20e8f721aa2248072d97
sdemianets <stanislav.demianets@amadeus.com>
2019-10-03 17:53:51 +0200

    Add form  add-event.components

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Category } from '../../shared/models/category.model';
+import { NgForm } from '@angular/forms';
+import { HBEvent } from '../../shared/models/event.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'hb-add-event',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-event.component.scss']
 })
 export class AddEventComponent implements OnInit {
+  @Input()
+  categories: Category[] = [];
+  types = [{ type: 'income', label: 'income' }, { type: 'outcome', label: 'outcome' }];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit(form: NgForm) {
+    const { description, category, type } = form.value;
+    const amount = form.value.amount < 0 ? form.value.amount * -1 : form.value.amount;
+    const date = moment().format('DD.MM.YYYY HH:mm:ss');
+    const event = new HBEvent(type, amount, +category, date, description);
   }
-
 }
--- src/app/system/shared/models/event.model.ts
+++ src/app/system/shared/models/event.model.ts
@@ -1,3 +1,10 @@
-export class Event {
-
+export class HBEvent {
+  constructor(
+    public type: string,
+    public amount: number,
+    public category: number,
+    public date: string,
+    public description: string,
+    public id?: number
+  ) {}
 }
