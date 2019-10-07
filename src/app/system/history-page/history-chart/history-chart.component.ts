3f72258c080b3f6d30456fc32f742590a5712bc8
sdemianets <stanislav.demianets@amadeus.com>
2019-10-07 12:58:48 +0200

    split history page on components

---
+++ src/app/system/history-page/history-chart/history-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hb-history-chart',
+  templateUrl: './history-chart.component.html',
+  styleUrls: ['./history-chart.component.scss']
+})
+export class HistoryChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -10,16 +10,20 @@ import { SystemComponent } from './system.component';
 import { AuthRoutingModule } from '../auth/auth-routing.module';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import {DropdownDirective} from './shared/directives/dropdown.directive';
+import { DropdownDirective } from './shared/directives/dropdown.directive';
 import { BillCardComponent } from './bill-page/bill-card/bill-card.component';
 import { CurrencyCardComponent } from './bill-page/currency-card/currency-card.component';
-import {BillService} from './shared/services/bill.service';
-import {MomentPipe} from './shared/pipes/moment.pipe';
+import { BillService } from './shared/services/bill.service';
+import { MomentPipe } from './shared/pipes/moment.pipe';
 import { AddEventComponent } from './records-page/add-event/add-event.component';
 import { AddCategoryComponent } from './records-page/add-category/add-category.component';
 import { EditCategoryComponent } from './records-page/edit-category/edit-category.component';
-import {CategoriesService} from './shared/services/categories.service';
-import {EventsService} from './shared/services/events.service';
+import { CategoriesService } from './shared/services/categories.service';
+import { EventsService } from './shared/services/events.service';
+import { HistoryChartComponent } from './history-page/history-chart/history-chart.component';
+import { HistoryEventsComponent } from './history-page/history-events/history-events.component';
+import { HistoryDetailComponent } from './history-page/history-detail/history-detail.component';
+import { HistoryFilterComponent } from './history-page/history-filter/history-filter.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, AuthRoutingModule, SystemRoutingModule],
@@ -37,7 +41,11 @@ import {EventsService} from './shared/services/events.service';
     MomentPipe,
     AddEventComponent,
     AddCategoryComponent,
-    EditCategoryComponent
+    EditCategoryComponent,
+    HistoryChartComponent,
+    HistoryEventsComponent,
+    HistoryDetailComponent,
+    HistoryFilterComponent
   ],
   providers: [BillService, CategoriesService, EventsService]
 })
