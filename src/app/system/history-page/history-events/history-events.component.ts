f79aceef3fe04316daad09e1a1949d2b054e2eaa
sdemianets <stanislav.demianets@amadeus.com>
2019-10-07 17:56:07 +0200

    Implement history-list page

--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Category } from '../../shared/models/category.model';
+import { HBEvent } from '../../shared/models/event.model';
 
 @Component({
   selector: 'hb-history-events',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history-events.component.scss']
 })
 export class HistoryEventsComponent implements OnInit {
+  @Input()
+  categories: Category[] = [];
+  @Input()
+  events: HBEvent[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.events.forEach(e => {
+      e.catName = this.categories.find(c => c.id === e.category).name;
+    });
   }
 
+  getEventClass(event: HBEvent) {
+    return {
+      label: true,
+      'label-danger': event.type === 'outcome',
+      'label-success': event.type === 'income'
+    };
+  }
 }
