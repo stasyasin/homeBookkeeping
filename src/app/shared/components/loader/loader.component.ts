3d34df0c4e4875262781db22408099e01d063779
sdemianets <stanislav.demianets@amadeus.com>
2019-10-10 15:58:26 +0200

    Add Loader

---
+++ src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'hb-loader',
+  template: `<div class="loader-animator"></div>`,
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent {
+
+}
