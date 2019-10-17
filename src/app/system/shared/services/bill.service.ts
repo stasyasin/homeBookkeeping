616352b55e075f49e5b4564f17c55a34ec4050f7
sdemianets <stanislav.demianets@amadeus.com>
2019-10-17 16:21:43 +0200

    fixes for firebase

--- src/app/system/shared/services/bill.service.ts
+++ src/app/system/shared/services/bill.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Bill } from '../models/bill.model';
 import { map } from 'rxjs/operators';
-import {BaseApi} from '../../../shared/core/base-api';
+import { BaseApi } from '../../../shared/core/base-api';
 
 @Injectable()
 export class BillService extends BaseApi {
@@ -13,13 +13,12 @@ export class BillService extends BaseApi {
   }
 
   getBill(): Observable<Bill> {
-    return this.get(`bill`).pipe(
-      map((bill: Bill) => bill)
-    );
+    return this.get(`bill`).pipe(map((bill: Bill) => bill));
   }
 
   getCurrency(base: string = 'UAH'): Observable<any> {
-    return this.http.get(`https://data.fixer.io/api/latest?access_key=${this.accessKey}`);
+    // not possible to use https on basic gixer subscription, when use http -> firebase provides some console error
+    return this.http.get(`http://data.fixer.io/api/latest?access_key=${this.accessKey}`);
   }
 
   updateBill(bill: Bill): Observable<Bill> {
