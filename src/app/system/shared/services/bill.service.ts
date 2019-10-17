516866267adcd1949792f88a03e06fab54885c92
sdemianets <stanislav.demianets@amadeus.com>
2019-10-17 16:48:06 +0200

    change to free exchangerate api

--- src/app/system/shared/services/bill.service.ts
+++ src/app/system/shared/services/bill.service.ts
@@ -16,9 +16,10 @@ export class BillService extends BaseApi {
     return this.get(`bill`).pipe(map((bill: Bill) => bill));
   }
 
-  getCurrency(base: string = 'UAH'): Observable<any> {
-    // not possible to use https on basic gixer subscription, when use http -> firebase provides some console error
-    return this.http.get(`http://data.fixer.io/api/latest?access_key=${this.accessKey}`);
+  getCurrency(base: string = 'EUR'): Observable<any> {
+    // return this.http.get(`http://data.fixer.io/api/latest?access_key=${this.accessKey}`); // // not possible to use https on basic fixer subscription, when use http -> firebase provides some console error
+    // return this.http.get(`https://api.exchangeratesapi.io/latest?base=${base}`); // good rest api, but no UAH
+    return this.http.get(`https://api.exchangerate-api.com/v4/latest/${base}`);
   }
 
   updateBill(bill: Bill): Observable<Bill> {
